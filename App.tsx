1baa7068464198cf7a3e83d249e92a25dc9c808c
seniorstack007 <seniorstack007@gmail.com>
2022-12-02 15:46:00 -0700

    s

--- App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { roomData } from './SampleRoom';
-import { Viewer } from './components';
-
-
-export default function App() {
-	return <Viewer data={roomData} />;
-}
